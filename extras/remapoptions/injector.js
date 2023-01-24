6f7195c3444db01104872fa0df0f82c199818f63
Leo Ueno <leoleoueno@gmail.com>
2023-01-24 16:21:51 +0000

    Added remap options and fixed issues to handle multiple extras

---
+++ extras/remapoptions/injector.js
@@ -0,0 +1,23 @@
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('extras/annotationsettings/script.js');
+s.onload = function() {
+  this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
+console.log("Script injected")
+
+async function setSync(key,value) {
+  return new Promise((resolve,reject)=>{
+    chrome.storage.sync.set({key: value},()=>{
+      resolve()
+    })
+  })
+}
+
+async function getSync(key) {
+  return new Promise((resolve,reject)=>{
+    chrome.storage.sync.get(key,(data)=>{
+      resolve(data)
+    })
+  })
+} 
