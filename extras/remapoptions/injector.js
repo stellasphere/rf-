3edbd93aa25b0e58f7af04a419cee3cc9d8eab07
Leo Ueno <leoleoueno@gmail.com>
2023-01-28 06:19:43 +0000

    Updated script

--- extras/remapoptions/injector.js
+++ extras/remapoptions/injector.js
@@ -5,19 +5,3 @@ s.onload = function() {
 };
 (document.head || document.documentElement).appendChild(s);
 console.log("Script injected",s.src)
-
-async function setSync(key,value) {
-  return new Promise((resolve,reject)=>{
-    chrome.storage.sync.set({key: value},()=>{
-      resolve()
-    })
-  })
-}
-
-async function getSync(key) {
-  return new Promise((resolve,reject)=>{
-    chrome.storage.sync.get(key,(data)=>{
-      resolve(data)
-    })
-  })
-} 
